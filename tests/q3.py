3f1841c262af5d93b3b1105a5bbae9c4a786ce06
Charles Frye <charles@wandb.com>
2022-03-04 15:20:47 -0800

    updates

---
+++ tests/q3.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q3',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': '>>> \n>>> import subprocess\n>>> \n>>> result = subprocess.run(["python", "foo.py"], capture_output=True)\n>>> result.stdout\nb\'Hello, world!\\n\'',
+                                       'hidden': False,
+                                       'locked': False,
+                                       'points': 1,
+                                       'success_message': 'Good job!'}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
